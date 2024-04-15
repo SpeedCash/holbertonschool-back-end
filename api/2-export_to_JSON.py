bdc9a8b20762155360a4ec5b5af4b171ed714871
SpeedCash <tcravero@gmail.com>
2024-04-15 09:01:29 +0000

    Using what you did in the task #0, extend your Python script to export data in the JSON format.

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+import json
+
+
+def main():
+    if len(sys.argv) < 2:
+        return
+    user_id = int(sys.argv[1])
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+
+    with urllib.request.urlopen(user_url) as response:
+        user = json.loads(response.read().decode())
+    with urllib.request.urlopen(todos_url) as response:
+        todos = json.loads(response.read().decode())
+
+    user_tasks = []
+    for task in todos:
+        task_info = {
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user.get('username')
+        }
+        user_tasks.append(task_info)
+
+    tasks_dict = {str(user_id): user_tasks}
+
+    file_name = f"{user_id}.json"
+    with open(file_name, 'w') as json_file:
+        json.dump(tasks_dict, json_file)
+
+
+if __name__ == "__main__":
+    main()
