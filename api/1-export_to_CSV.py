97a6f1190b02fe1b146fe9924164e89f8b3d529b
SpeedCash <tcravero@gmail.com>
2024-04-15 08:54:54 +0000

    Using what you did in the task #0, extend your Python script to export data in the CSV format.

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+import json
+import csv
+
+
+def main():
+    if len(sys.argv) < 2:
+        return
+    user_id = int(sys.argv[1])
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+
+    with urllib.request.urlopen(user_url) as response:
+        user = json.loads(response.read().decode())
+    with urllib.request.urlopen(todos_url) as response:
+        todos = json.loads(response.read().decode())
+
+    file_name = f"{user_id}.csv"
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([user_id, user.get('username'),
+                             task.get('completed'), task.get('title')])
+
+
+if __name__ == "__main__":
+    main()
